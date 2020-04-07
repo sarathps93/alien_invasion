6bc816b5c02abcb98f5390486bf031a0a5baf60d
Sarath P S <sarathps93@gmail.com>
2020-04-07 14:42:11 +0530

    feat(*): alient movement

--- alien.py
+++ alien.py
@@ -14,8 +14,14 @@ class Alien(Sprite):
         self.rect.y = self.rect.height
 
         self.x = float(self.rect.x)
+    
+    def check_edges(self):
+        # Return True if alien is at the edge of screen
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
 
     def update(self):
         # Move alien to the right
-        self.x += self.settings.alien_speed
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
         self.rect.x = self.x
--- alien_invasion.py
+++ alien_invasion.py
@@ -99,7 +99,20 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
 
     def _update_aliens(self):
+        self._check_fleet_edges()
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        # Drop the entire fleet and change the fleet direction
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
 if __name__ == '__main__':
     ai = AlienInvasion()
