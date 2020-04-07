3659eafa6b401e5c7dd2ebeea9861d0d2f669a4d
Sarath P S <sarathps93@gmail.com>
2020-04-07 17:58:32 +0530

    feat(*): added number of lives left and finished the game

--- alien_invasion.py
+++ alien_invasion.py
@@ -157,6 +157,7 @@ class AlienInvasion:
         if self.stats.ships_left > 0:
             # Decrement ships left
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
             # Get rid of remaining aliens and bullets
             self.aliens.empty()
             self.bullets.empty()
@@ -187,6 +188,8 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
             # Get rid of aliens and bullets remaining
             self.aliens.empty()
             self.bullets.empty()
